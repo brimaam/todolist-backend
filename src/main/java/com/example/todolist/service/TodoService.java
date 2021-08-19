a05a23fa6021ee0a381e5cc5ac848245a0195aa3
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-19 12:23:50 +0800

    [Todo-Backend][AmeerBrima] feat: added todo service and request

---
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -0,0 +1,22 @@
+package com.example.todolist.service;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+}
