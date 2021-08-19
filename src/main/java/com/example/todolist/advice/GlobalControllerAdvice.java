e650ac736b612fd6f97c3891984ce009967713ce
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-19 15:04:42 +0800

    [Todo-Backend][AmeerBrima] feat: added exception handling for todo not found

---
+++ src/main/java/com/example/todolist/advice/GlobalControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.example.todolist.advice;
+
+import com.example.todolist.exception.TodoNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler
+    public ErrorResponse todoNotFoundExceptionHandling(TodoNotFoundException todoNotFoundException) {
+        return new ErrorResponse(todoNotFoundException.getMessage());
+    }
+}
